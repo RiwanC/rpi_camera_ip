815c29ff74b6baaa51daf673df4f1cf5756e006e
Caine Silva <caine.silva@gmx.com>
2020-01-10 08:16:41 +0100

    simple server architecture

--- Client/Main.c
+++ Client/Main.c
@@ -1,4 +1,49 @@
-//
-// Created by Dymitry on 07/01/2020.
-//
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <strings.h>
+#include <unistd.h>
+#include <netdb.h>
 
+typedef struct socketPack {
+    int socket;
+    struct sockaddr_in sin;
+} SOCKET_PACK;
+
+SOCKET_PACK* init_socket(int port, char* address){
+    int socket_service = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_service == -1){
+        perror("Unable to create the socket");
+        exit(1);
+    }
+    struct sockaddr_in sin;
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons(port);
+    struct hostent* host, *gethostbyname();
+    host = gethostbyname(address);
+    bcopy(host->h_addr, &sin.sin_addr.s_addr,host->h_length);
+    SOCKET_PACK* socket_pack = malloc(sizeof(SOCKET_PACK));
+    socket_pack->socket = socket_service;
+    socket_pack->sin = sin;
+    return socket_pack;
+}
+
+int main(int argc, char* argv[]){
+    if (argc != 3){
+        printf("Usage: ./client address port");
+        exit(1);
+    }
+    SOCKET_PACK* pack = init_socket(atoi(argv[2]), argv[1]);
+    if (connect(pack->socket,(struct sockaddr*) &(pack->sin), sizeof(pack->sin)) != 0){
+        printf("Cannot connect to server\n");
+        exit(1);
+    }
+    char buf[256] = "\0";
+    fgets(buf, 256*sizeof(char), stdin);
+    printf("%s\n",buf);
+    buf[255] = '\0';
+    write(pack->socket, buf, 256*sizeof(char));
+    exit(0);
+}
--- Server/Main.c
+++ Server/Main.c
@@ -1,4 +1,58 @@
-//
-// Created by Dymitry on 07/01/2020.
-//
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <strings.h>
+#include <unistd.h>
 
+typedef struct socketPack {
+    int socket;
+    struct sockaddr_in sin;
+} SOCKET_PACK;
+
+SOCKET_PACK* initSocketServer(int port){
+    int sock_RV = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock_RV == -1){
+        perror("Unable to create the socket");
+        exit(1);
+    }
+    struct sockaddr_in sin;
+    sin.sin_addr.s_addr = htonl(INADDR_ANY);
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons(port);
+    if (bind(sock_RV, (struct sockaddr*) &sin, sizeof(sin)) == -1){
+        perror("Unable to bind socket");
+        exit(1);
+    }
+    if (listen(sock_RV,5) == -1){
+        perror("Unable to listen on the socket");
+        exit(1);
+    }
+    SOCKET_PACK* pack = malloc(sizeof(SOCKET_PACK));
+    pack->socket = sock_RV;
+    pack->sin = sin;
+    return pack;
+}
+
+void waitForConnection(SOCKET_PACK* packRV){
+    socklen_t length = sizeof(packRV->sin);
+    int socket = accept(packRV->socket, (struct sockaddr*) &(packRV->sin), &length);
+    printf("Connection of client\n");
+    char buf[256] = "\0";
+    read(socket, buf, 255*sizeof(char));
+    buf[255] = '\0';
+    printf("%s is connected",buf);
+    fflush(stdin);
+}
+
+int main(int argc, char* argv[]){
+    if (argc != 2){
+        printf("Usage ./server port\n");
+        exit(1);
+    }
+    SOCKET_PACK* packRV = initSocketServer(atoi(argv[1]));
+    while (1){
+        waitForConnection(packRV);
+    }
+}
