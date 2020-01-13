6bbb0d2fe4fc50bc8845ed974f9a72b6ac149d9d
Kevin Bedin <kbedin@iri.upc.edu>
2020-01-13 21:13:08 +0100

    SingleCapture.c repaired | IHM created

---
+++ Client/ihm.c
@@ -0,0 +1,191 @@
+/* 
+To compile:
+gcc -g ihm.c -o IHM  `pkg-config --libs --cflags gtk+-3.0 gmodule-2.0`
+
+Official documentation:
+https://developer.gnome.org/gtk3/stable/
+
+*/
+
+#include <gtk/gtk.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define OK 1
+#define KO 0
+
+#define TAKE 1
+#define SAVE 2
+
+
+GtkWidget *main_window = NULL;
+GtkWidget *picture = NULL;  
+
+int status = KO;
+char IP_CAMERA[30]="192.168.0.1";
+
+void format_time(char *output){
+    time_t rawtime;
+    struct tm * timeinfo;
+
+    time ( &rawtime );
+    timeinfo = localtime ( &rawtime );
+
+/*    sprintf(output, "[%d/%d/%d-%dH:%d':%d'']",timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);*/
+    sprintf(output, "[ %.2dh%.2dm%.2ds ]", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+}
+
+void updateInfo(GtkLabel * InfLabel, int event, char * name)
+{
+    char info[1000];
+    char state[80];
+    char action[256];
+    char time[30];
+
+    format_time(time);
+
+    switch(status)
+    {
+    case OK: sprintf(state," • Camera state: Ready\n\n • IP Address: %s",IP_CAMERA); break;
+    case KO: sprintf(state," • Camera state: No connection\n\n • IP Address: No connection"); break;
+    }
+
+    switch(event)
+    {
+    case TAKE: sprintf(action, " • Last event:\n\n\t %s Picture taken", time); break;
+    case SAVE: sprintf(action, " • Last event: \n\n\t %s Picture saved\n\t(%.30s)", time, name); break;
+    default: sprintf(action, " • Last event:\n\n\t\t No event");
+    }
+
+    sprintf(info,"%s\n\n%s\n",state,action);
+    gtk_label_set_text(InfLabel,info);
+}
+
+int open_dialog(char * name_file)
+{
+    GtkWidget *dialog;
+
+    dialog = gtk_file_chooser_dialog_new("Choose a file:", GTK_WINDOW(main_window), 
+                         GTK_FILE_CHOOSER_ACTION_SAVE,
+                         ("_Cancel"),
+                         GTK_RESPONSE_CANCEL,
+                         ("_Save"),
+                         GTK_RESPONSE_OK, NULL);
+
+    gtk_file_chooser_set_current_name(GTK_FILE_CHOOSER(dialog), "image.jpg");
+    gtk_widget_show_all(dialog);
+    gint resp = gtk_dialog_run(GTK_DIALOG(dialog));
+    if (resp == GTK_RESPONSE_OK)
+    {
+        g_print("\tPath: %s\n", gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog)));
+        sprintf(name_file,"%s", gtk_file_chooser_get_current_name(GTK_FILE_CHOOSER(dialog)));
+        gtk_widget_destroy(dialog);
+        return 1;
+    }
+    else
+    {
+        g_print("\tSaving cancel\n");
+        gtk_widget_destroy(dialog);
+        return 0;
+    }
+}
+
+void takePicutre(GtkWidget *widget, gpointer data)
+{
+    printf("\nTake picture\n");
+    updateInfo((GtkLabel*) data, TAKE, NULL);
+/*    gtk_image_set_from_file((GtkImage *)picture,"1.png");*/
+/*    gtk_image_new_from_pixbuf ()*/
+}
+
+
+void savePicture(GtkWidget *widget, gpointer data)
+{
+    printf("\nSave picture\n");
+    char name_file[256];
+    int success = open_dialog(name_file);
+    if (success) updateInfo((GtkLabel*) data, SAVE, name_file);
+}
+
+void cameraDetection(GtkWidget *widget, gpointer data)
+{
+    printf("\nCamera detection started ...\n");
+}
+
+
+
+int ihm(int argc, char *argv [])
+{
+
+
+    GtkWidget *take_button = NULL;  
+    GtkWidget *save_button= NULL;
+    GtkWidget *detection_button= NULL;
+    GtkWidget *inf_label = NULL;  
+    GtkBuilder *builder = NULL;
+    gchar *filename = NULL;
+    GError *error = NULL;
+
+    /* Initialisation of the Gtk.library */
+    gtk_init(&argc, &argv);
+
+    /* Glade file opening*/
+    builder = gtk_builder_new();
+
+
+    /* g_build_filename(): get the path for the file according to the OS */
+    filename =  g_build_filename("IHM.glade", NULL);
+
+    /* Loading of the "ihm.glade" file */
+    gtk_builder_add_from_file(builder, filename, &error);
+    if (error)
+    {
+        gint code = error->code;
+        g_printerr("%s\n", error->message);
+        g_error_free (error);
+        return code;
+    }
+
+    /* Getting of main window pointer */
+    main_window = GTK_WIDGET(gtk_builder_get_object(builder, "window1"));
+    if (NULL == main_window)
+    {
+        /* Print out the error. You can use GLib's message logging  */
+        fprintf(stderr, "Unable to file object with id \"window1\" \n");
+        /* Your error handling code goes here */
+    }
+    else
+    {
+        /* Widget getting */
+
+        take_button = GTK_WIDGET(gtk_builder_get_object(builder, "take_button"));
+        save_button = GTK_WIDGET(gtk_builder_get_object(builder, "save_button"));
+        detection_button = GTK_WIDGET(gtk_builder_get_object(builder, "detection_button"));
+        inf_label = GTK_WIDGET(gtk_builder_get_object(builder, "information_label"));
+        picture = GTK_WIDGET(gtk_builder_get_object(builder, "picture"));
+
+        /* Callback function assignment */
+        g_signal_connect (G_OBJECT (main_window), "destroy", G_CALLBACK (gtk_main_quit), NULL);
+        g_signal_connect(G_OBJECT(save_button), "clicked", G_CALLBACK(savePicture), inf_label);
+        g_signal_connect(G_OBJECT(take_button), "clicked", G_CALLBACK(takePicutre), inf_label);
+        g_signal_connect(G_OBJECT(detection_button), "clicked", G_CALLBACK(cameraDetection), inf_label);
+
+        /* Display the main window */
+        gtk_widget_show_all(main_window);
+        g_free (filename);
+
+        updateInfo((GtkLabel*) inf_label, -1, NULL);
+
+        gtk_main();
+    }
+
+    return 0;
+}
+
+int main(int argc, char *argv [])
+{
+    ihm(argc, argv);
+    return EXIT_SUCCESS;
+}
--- Server/SingleCapture.c
+++ Server/SingleCapture.c
@@ -1,6 +1,3 @@
-//
-// Created by Riwan on 07/01/2020.
-//
 
 #include <errno.h>
 #include <fcntl.h>
@@ -10,152 +7,147 @@
 #include <string.h>
 #include <sys/ioctl.h>
 #include <sys/mman.h>
+#include <libv4l2.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <jpeglib.h>
 
-uint8_t *buffer;
+
+
+#define CLEAR(x) memset (&(x), 0, sizeof (x))
+
+// minimum number of buffers to request in VIDIOC_REQBUFS call
+#define VIDIOC_REQBUFS_COUNT 2
+
+struct buffer {
+        void *                  start;
+        size_t                  length;
+};
+
+static unsigned int     n_buffers       = 0;
+struct buffer *         buffers         = NULL;
+
+
+
+static char* deviceName = "/dev/video0";
+static unsigned int width = 640;
+static unsigned int height = 480;
+static unsigned char jpegQuality = 120;
+
+
+
+/* Fonction from yuv.c */
+void YUV420toYUV444(int width, int height, unsigned char* src, unsigned char* dst) {
+    int line, column;
+    unsigned char *py, *pu, *pv;
+    unsigned char *tmp = dst;
+
+    // In this format each four bytes is two pixels. Each four bytes is two Y's, a Cb and a Cr.
+    // Each Y goes to one of the pixels, and the Cb and Cr belong to both pixels.
+    unsigned char *base_py = src;
+    unsigned char *base_pu = src+(height*width);
+    unsigned char *base_pv = src+(height*width)+(height*width)/4;
+
+    for (line = 0; line < height; ++line) {
+        for (column = 0; column < width; ++column) {
+            py = base_py+(line*width)+column;
+            pu = base_pu+(line/2*width/2)+column/2;
+            pv = base_pv+(line/2*width/2)+column/2;
+
+            *tmp++ = *py;
+            *tmp++ = *pu;
+            *tmp++ = *pv;
+        }
+    }
+}
+
+
+
+static void errno_exit(const char* s)
+{
+    fprintf(stderr, "%s error %d, %s\n", s, errno, strerror(errno));
+    exit(EXIT_FAILURE);
+}
+
+
+
 
 //check if the capture is available or not
 static int xioctl(int fd, int request, void *arg)
 {
     //wrapper function over ioct.ioctl() which is a function to manipulate device parameters of special files
     int r;
-    do r = ioctl (fd, request, arg);
+    do{
+         r = ioctl (fd, request, arg);
+    }
     while (-1 == r && EINTR == errno);
     return r;
 }
 
-//retrieve camera capabilities
-int print_caps(int fd)
+
+
+
+
+static void jpegWrite(unsigned char* img, char* jpegFilename)
 {
-    struct v4l2_capability caps = {};
-    if (-1 == xioctl(fd, VIDIOC_QUERYCAP, &caps))
-    {
-        perror("Pb querying Capabilities");
-        return 1;
+    struct jpeg_compress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+
+    JSAMPROW row_pointer[1];
+    FILE *outfile = fopen( jpegFilename, "wb" );
+
+    // try to open file for saving
+    if (!outfile) {
+        errno_exit("jpeg");
     }
 
-    printf( "Driver Caps:\n"
-            "  Driver: \"%s\"\n"
-            "  Card: \"%s\"\n"
-            "  Bus: \"%s\"\n"
-            "  Version: %d.%d\n"
-            "  Capabilities: %08x\n",
-            caps.driver,
-            caps.card,
-            caps.bus_info,
-            (caps.version>>16)&&0xff,
-            (caps.version>>24)&&0xff,
-            caps.capabilities);
+    // create jpeg data
+    cinfo.err = jpeg_std_error( &jerr );
+    jpeg_create_compress(&cinfo);
+    jpeg_stdio_dest(&cinfo, outfile);
 
+    // set image parameters
+    cinfo.image_width = width;
+    cinfo.image_height = height;
+    cinfo.input_components = 3;
+    cinfo.in_color_space = JCS_YCbCr;
 
-    struct v4l2_cropcap cropcap = {0};
-    cropcap.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    // set jpeg compression parameters to default
+    jpeg_set_defaults(&cinfo);
+    // and then adjust quality setting
+    jpeg_set_quality(&cinfo, jpegQuality, TRUE);
 
-    if (-1 == xioctl (fd, VIDIOC_CROPCAP, &cropcap))
-    {
-        perror("Pb querying Cropping Capabilities");
-        return 1;
+    // start compress
+    jpeg_start_compress(&cinfo, TRUE);
+
+    // feed data
+    while (cinfo.next_scanline < cinfo.image_height) {
+        row_pointer[0] = &img[cinfo.next_scanline * cinfo.image_width *  cinfo.input_components];
+        jpeg_write_scanlines(&cinfo, row_pointer, 1);
     }
 
-    printf( "Camera Cropping:\n"
-            "  Bounds: %dx%d+%d+%d\n"
-            "  Default: %dx%d+%d+%d\n"
-            "  Aspect: %d/%d\n",
-            cropcap.bounds.width, cropcap.bounds.height, cropcap.bounds.left, cropcap.bounds.top,
-            cropcap.defrect.width, cropcap.defrect.height, cropcap.defrect.left, cropcap.defrect.top,
-            cropcap.pixelaspect.numerator, cropcap.pixelaspect.denominator);
+    // finish compression
+    jpeg_finish_compress(&cinfo);
 
-    int support_grbg10 = 0;
+    // destroy jpeg data
+    jpeg_destroy_compress(&cinfo);
 
-    struct v4l2_fmtdesc fmtdesc = {0};
-    fmtdesc.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    char fourcc[5] = {0};
-    char c, e;
-    printf("  FMT : CE Desc\n--------------------\n");
-    while (0 == xioctl(fd, VIDIOC_ENUM_FMT, &fmtdesc))
-    {
-        strncpy(fourcc, (char *)&fmtdesc.pixelformat, 4);
-        if (fmtdesc.pixelformat == V4L2_PIX_FMT_SGRBG10)
-            support_grbg10 = 1;
-        c = fmtdesc.flags & 1? 'C' : ' ';
-        e = fmtdesc.flags & 2? 'E' : ' ';
-        printf("  %s: %c%c %s\n", fourcc, c, e, fmtdesc.description);
-        fmtdesc.index++;
-    }
-
-    if (!support_grbg10)
-    {
-        printf("Doesn't support GRBG10.\n");
-        return 1;
-    }
-
-    //V4L2 provides an easy interface to check the image formats and colorspace that the webcam supports and provide. v4l2_format structure is to be used to change image format.
-    struct v4l2_format fmt = {0};
-    fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    //image width and height according to what the camera supports + format (mjpeg, yuv, ...)
-    fmt.fmt.pix.width = 752;
-    fmt.fmt.pix.height = 480;
-    fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_SGRBG10;
-    fmt.fmt.pix.field = V4L2_FIELD_NONE;
-
-    if (-1 == xioctl(fd, VIDIOC_S_FMT, &fmt))
-    {
-        perror("Pb setting Pixel Format");
-        return 1;
-    }
-
-    strncpy(fourcc, (char *)&fmt.fmt.pix.pixelformat, 4);
-    printf( "Selected Camera Mode:\n"
-            "  Width: %d\n"
-            "  Height: %d\n"
-            "  PixFmt: %s\n"
-            "  Field: %d\n",
-            fmt.fmt.pix.width,
-            fmt.fmt.pix.height,
-            fourcc,
-            fmt.fmt.pix.field);
-
-    return 0;
+    // close output file
+    fclose(outfile);
 }
 
 
-int init_mmap(int fd)
-{
-    //A buffer contains data exchanged by application and driver using Streaming I/O methods. v4l2_requestbuffers is used to allocate device buffers.
-    struct v4l2_requestbuffers req = {0};
-    req.count = 1;
-    req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    req.memory = V4L2_MEMORY_MMAP;
 
-    if (-1 == xioctl(fd, VIDIOC_REQBUFS, &req))
-    {
-        perror("Pb requesting Buffer");
-        return 1;
-    }
 
-    //After requesting buffer from the device, we need to query the buffer in order to get raw data.
-    struct v4l2_buffer buf = {0};
-    buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    buf.memory = V4L2_MEMORY_MMAP;
-    buf.index = 0;
-    if(-1 == xioctl(fd, VIDIOC_QUERYBUF, &buf))
-    {
-        perror("Pb querying Buffer");
-        return 1;
-    }
-
-    //The mmap() function asks to map length bytes starting at offset in the memory of the device specified by fd into the application address space, preferably at address start.
-    buffer = mmap (NULL, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, buf.m.offset);
-    printf("Length: %d\nAddress: %p\n", buf.length, buffer);
-    printf("Image Length: %d\n", buf.bytesused);
-
-    return 0;
-}
 
 //capture image
 int capture_image(int fd)
 {
+    unsigned char* src;
+    unsigned char* dst = malloc(width*height*3*sizeof(char));
     struct v4l2_buffer buf = {0};
+
+
     buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
     buf.memory = V4L2_MEMORY_MMAP;
     buf.index = 0;
@@ -164,6 +156,7 @@ int capture_image(int fd)
         perror("Pb querying Buffer");
         return 1;
     }
+
 
     //After querying the buffer, the only thing left is capturing the frame and saving it in the buffer.
     if(-1 == xioctl(fd, VIDIOC_STREAMON, &buf.type))
@@ -190,19 +183,173 @@ int capture_image(int fd)
         return 1;
     }
 
-    //store data
-    int outfd = open("out.img", O_RDWR);
-    write(outfd, buffer, buf.bytesused);
-    close(outfd);
+    //store data - YUV420
+/*    int outfd = open("out.img", O_WRONLY|O_CREAT);*/
+/*    write(outfd, buffers, buf.bytesused);*/
+/*    close(outfd);*/
+
+    //store data - YUV440 / JPEG
+    src = (unsigned char*) buffers[0].start;
+
+
+    YUV420toYUV444(width, height, src, dst);
+
+    jpegWrite(dst, "image.jpg");
 
     return 0;
 }
 
+
+
+
+
+
+//retrieve camera capabilities
+int print_caps(int fd)
+{
+    struct v4l2_capability caps = {};
+    if (-2 == xioctl(fd, VIDIOC_QUERYCAP, &caps))
+    {
+        perror("Pb querying Capabilities");
+        return 1;
+    }
+
+    printf( "Driver Caps:\n"
+            "  Driver: \"%s\"\n"
+            "  Card: \"%s\"\n"
+            "  Bus: \"%s\"\n"
+            "  Version: %d.%d\n"
+            "  Capabilities: %08x\n",
+            caps.driver,
+            caps.card,
+            caps.bus_info,
+            (caps.version>>16)&&0xff,
+            (caps.version>>24)&&0xff,
+            caps.capabilities);
+
+
+    struct v4l2_cropcap cropcap = {0};
+    cropcap.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+
+    if (0 == xioctl(fd, VIDIOC_CROPCAP, &cropcap)) {
+
+    }
+
+    printf( "Camera Cropping:\n"
+            "  Bounds: %dx%d+%d+%d\n"
+            "  Default: %dx%d+%d+%d\n"
+            "  Aspect: %d/%d\n",
+            cropcap.bounds.width, cropcap.bounds.height, cropcap.bounds.left, cropcap.bounds.top,
+            cropcap.defrect.width, cropcap.defrect.height, cropcap.defrect.left, cropcap.defrect.top,
+            cropcap.pixelaspect.numerator, cropcap.pixelaspect.denominator);
+
+
+    struct v4l2_fmtdesc fmtdesc = {0};
+    fmtdesc.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    char fourcc[5] = {0};
+    char c, e;
+    printf("  FMT : CE Desc\n--------------------\n");
+    while (0 == xioctl(fd, VIDIOC_ENUM_FMT, &fmtdesc))
+    {
+        strncpy(fourcc, (char *)&fmtdesc.pixelformat, 4);
+        c = fmtdesc.flags & 1? 'C' : ' ';
+        e = fmtdesc.flags & 2? 'E' : ' ';
+        printf("  %s: %c%c %s\n", fourcc, c, e, fmtdesc.description);
+        fmtdesc.index++;
+    }
+
+    //V4L2 provides an easy interface to check the image formats and colorspace that the webcam supports and provide. v4l2_format structure is to be used to change image format.
+    struct v4l2_format fmt = {0};
+    fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    //image width and height according to what the camera supports + format (mjpeg, yuv, ...)
+    fmt.fmt.pix.width = width;
+    fmt.fmt.pix.height = height;
+    fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_YUV420;
+    fmt.fmt.pix.field = V4L2_FIELD_INTERLACED;
+
+
+    strncpy(fourcc, (char *)&fmt.fmt.pix.pixelformat, 4);
+    printf( "Selected Camera Mode:\n"
+            "  Width: %d\n"
+            "  Height: %d\n"
+            "  PixFmt: %s\n"
+            "  Field: %d\n",
+            fmt.fmt.pix.width,
+            fmt.fmt.pix.height,
+            fourcc,
+            fmt.fmt.pix.field);
+
+    return 0;
+}
+
+
+
+
+
+static void mmapInit(int fd)
+{
+    struct v4l2_requestbuffers req;
+
+    CLEAR(req);
+
+    req.count = VIDIOC_REQBUFS_COUNT;
+    req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    req.memory = V4L2_MEMORY_MMAP;
+
+    if (-1 == xioctl(fd, VIDIOC_REQBUFS, &req)) {
+        if (EINVAL == errno) {
+            fprintf(stderr, "%s does not support memory mapping\n", deviceName);
+            exit(EXIT_FAILURE);
+        } else {
+            errno_exit("VIDIOC_REQBUFS");
+        }
+    }
+
+    if (req.count < 2) {
+        fprintf(stderr, "Insufficient buffer memory on %s\n", deviceName);
+        exit(EXIT_FAILURE);
+    }
+
+    buffers = calloc(req.count, sizeof(*buffers));
+
+    if (!buffers) {
+        fprintf(stderr, "Out of memory\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (n_buffers = 0; n_buffers < req.count; ++n_buffers) {
+        struct v4l2_buffer buf;
+
+        CLEAR(buf);
+
+        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        buf.memory = V4L2_MEMORY_MMAP;
+        buf.index = n_buffers;
+
+        if (-1 == xioctl(fd, VIDIOC_QUERYBUF, &buf))
+            errno_exit("VIDIOC_QUERYBUF");
+
+        buffers[n_buffers].length = buf.length;
+        buffers[n_buffers].start = v4l2_mmap(NULL /* start anywhere */, buf.length, PROT_READ | PROT_WRITE /* required */, MAP_SHARED /* recommended */, fd, buf.m.offset);
+        printf("Length: %d\nAddress: %p\n", buf.length, buffers);
+        printf("Image Length: %d\n", buf.bytesused);
+
+
+        if (MAP_FAILED == buffers[n_buffers].start)
+            errno_exit("mmap");
+    }
+}
+
+
+
+
+
 int main()
 {
-    int fd;
+
     //open the capture device. Default capture device is /dev/video0
-    fd = open("/dev/video0", O_RDWR);
+    int fd = v4l2_open(deviceName, O_RDWR /* required */ | O_NONBLOCK, 0);
+/*    fd = open("/dev/video0", O_RDWR);*/
     if (fd == -1)
     {
         //could not find capture device
@@ -214,12 +361,11 @@ int main()
     if(print_caps(fd))
         return 1;
 
-    if(init_mmap(fd))
-        return 1;
+    mmapInit(fd);
+
 
     if(capture_image(fd))
         return 1;
-    ////////////////////////////
 
     //close the capture device
     close(fd);
